594b6a7f5e2244a3d2590cdb578ae46d24029117
luis <luis.gil@tajamar365.com>
2015-10-07 12:06:42 +0200

    fin

---
+++ WorkerReloj/index.js
@@ -0,0 +1,30 @@
+﻿var worker = undefined;
+function CrearWorker() {
+    worker = new Worker("reloj.js");
+    worker.onmessage=function(evt) {
+        var capa = document.getElementById("hora");
+        if (evt.data.accion === 1) {
+            capa.innerHTML = evt.data.contenido;
+
+        }
+        else if (evt.data.accion === 2) {
+            capa.style.color = evt.data.contenido;
+        }
+    }
+}
+function cambiarColor() {
+    var data = { accion: 2 };
+    worker.postMessage(data);
+
+}
+function cambiarZona() {
+    var data = { accion: 1 };
+    worker.postMessage(data);
+}
+
+CrearWorker();
+document.getElementById("btnColor")
+    .addEventListener("click", cambiarColor);
+document.getElementById("btnZona")
+    .addEventListener("click", cambiarZona);
+
---
+++ WorkerReloj/reloj.js
@@ -0,0 +1,39 @@
+﻿var insular = false;
+
+function hora() {
+
+    var d = new Date();
+
+    var h = (!insular ? d.getHours() : (d.getHours() - 1)) + ":"
+        + d.getMinutes() + ":" + d.getSeconds();
+
+    postMessage({ accion: 1, contenido: h });
+    setTimeout("hora()", 500);
+
+}
+function color() {
+    var r = parseInt(Math.random() * 255);
+    var g = parseInt(Math.random() * 255);
+    var b = parseInt(Math.random() * 255);
+    
+    var col = "rgb(" + r+"," + g+"," + b+")";
+    postMessage({ accion: 2, contenido: col });
+}
+function zona() {
+    if (insular === true)
+        insular = false;
+    else 
+        insular = true;
+  //  insular = insular?false:true;
+}
+
+hora();
+
+self.onmessage=function(evt) {
+    if (evt.data.accion === 1) {
+        zona();
+    }
+    else if (evt.data.accion === 2)
+        color();
+
+}
